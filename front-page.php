6c58cae52b97b81baa0fb5e5efbd7b396ff9c7af
melissacabral <melissacabral@gmail.com>
2014-06-10 17:44:52 -0700

    after day 3 - basic theme and styling

---
+++ front-page.php
@@ -0,0 +1,33 @@
+<?php get_header(); //include header.php ?>
+
+<main id="content">
+	<?php //THE LOOP
+		if( have_posts() ): ?>
+		<?php while( have_posts() ): the_post(); ?>
+
+		<?php the_post_thumbnail('banner'); //show the featured image ?>
+
+		<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
+			<h2 class="entry-title"> 
+				<a href="<?php the_permalink(); ?>"> 
+					<?php the_title(); ?> 
+				</a>
+			</h2>
+			<div class="entry-content">
+				<?php the_content(); ?>
+			</div>
+				
+		</article><!-- end post -->
+
+		<?php endwhile; ?>
+	<?php else: ?>
+
+	<h2>Sorry, no posts found</h2>
+	<p>Try using the search bar instead</p>
+
+	<?php endif;  //end THE LOOP ?>
+
+</main><!-- end #content -->
+
+<?php get_sidebar(); //include sidebar.php ?>
+<?php get_footer(); //include footer.php ?>
---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+//turn on sleeping features
+add_theme_support('post-thumbnails');
+
+//add a custom image size
+// $name, $width, $height, $crop
+add_image_size( 'banner', 1300, 300, true );
+
+//DO NOT CLOSE PHP
